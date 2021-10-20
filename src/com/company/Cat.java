e2ceb5704d3be0d954a1190425587e52822ff1e3
Dmitriy Antonov <79038945169@yandex.ru>
2021-10-20 17:59:30 +0300

    Initial commit

---
+++ src/com/company/Cat.java
@@ -0,0 +1,159 @@
+package com.company;
+
+public class Cat implements Cloneable
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    //для УРОКА №3 статическое поле
+    private static int count=0;
+
+    //УРОК №4, создадим константы
+    final static double MIN_WEIGHT = 1000.0;
+    final static double MAX_WEIGHT = 9000.0;
+    //ENUM с окрасами
+    enum ColorOfTheCat {
+        BlackTabby, FawnTarry, BlueTabby, ChocolateTabby
+    }
+    // геттеры и сеттеры для окраса кошки для урока №6
+    public ColorOfTheCat getColorOfTheCat() {
+        return colorOfTheCat;
+    }
+
+    public void setColorOfTheCat(ColorOfTheCat colorOfTheCat) {
+        this.colorOfTheCat = colorOfTheCat;
+    }
+
+    //Урок №6
+    //переменная для окраса
+    private ColorOfTheCat colorOfTheCat;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        //Используем константы из урока №4
+        //minWeight = 1000.0;
+        //maxWeight = 9000.0;
+        minWeight = MIN_WEIGHT;
+        maxWeight = MAX_WEIGHT;
+
+        //увеличиваем значение count в момент создания кошки
+        //делаем кошку "живой"
+        count++;
+    }
+
+    // Создадим конструктор для урока №5
+
+    public Cat(double weight) {
+        this.weight = weight;
+        originWeight = weight;
+    }
+
+    public void meow()
+    {
+        //запретим мяукать неживой кошке
+        if (weight<minWeight) {
+            System.out.println("Неживая кошка не может мяукать");
+        }
+        else {
+            weight = weight - 1;
+            System.out.println("Meow");
+        }
+    }
+
+    public void feed(Double amount)
+    {
+        //запретим есть неживой кошке
+        if (weight<minWeight) {
+            System.out.println("Нельзя кормить неживую кошку");
+        }
+        else weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        //запретим пить неживой кошке
+        if (weight<minWeight){
+           System.out.println("Нельзя поить неживую кошку");
+        }
+        else weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+    //методы для второго урока
+    //возврат суммы съеденной еды
+    public double getFoodEaten(){
+        return weight-originWeight;
+    }
+    //метод "сходить в туалет"
+    public void pee(){
+        //запретим ходить в туалет неживой кошке
+        if (weight<minWeight) {
+            System.out.println("Неживая кошка не может ходить в туалет");
+        }
+        else {
+            originWeight-=10;
+            weight-=10;
+            System.out.println("Хозяин, я сходила в туалет, мяу!");
+        }
+    }
+
+    //УРОК №3
+    //метод, который возвращает количество кошек
+    public static int getCount(){
+        return count;
+    }
+    //метод проверки "состояния" кошки
+    public void aliveOrNot(){
+        if (weight<minWeight){
+            count--;
+        }
+    }
+    public Cat(double originWeight, double weight, double minWeight, double maxWeight, ColorOfTheCat colorOfTheCat) {
+        this.originWeight = originWeight;
+        this.weight = weight;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+        this.colorOfTheCat = colorOfTheCat;
+    }
+    public double getOriginWeight() {
+        return originWeight;
+    }
+    //Урок №7
+    // создадим метод для "копирования" кошки,
+    // переопределив метод clone()
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    //а также конструктор для копирования кошки
+    public Cat(Cat cat)
+    {
+       this(cat.originWeight,cat.getWeight(),MIN_WEIGHT,MAX_WEIGHT,cat.getColorOfTheCat());
+    }
+}
---
+++ src/com/company/Loader.java
@@ -0,0 +1,130 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        //УРОК №1
+
+        //создадим 7 кошек, можно отдельно, но короче массивом и циклом
+        //и сразу выведем в консоль их вес
+        Cat[] cats = new Cat[7];
+        for(int i=0;i<7;i++){
+            cats[i] = new Cat();
+            System.out.println("Вес кошки №" + (i+1) + " " + cats[i].getWeight());
+            //округлим и оставим 2 знака после запятой
+            System.out.print("Вес кошки №" + (i+1) + " ");
+            System.out.printf("%.2f",cats[i].getWeight());
+            System.out.println();
+        }
+        //покормим первую и последнюю кошку
+        cats[0].feed(150.0);
+        cats[6].feed(250.0);
+        //новый увеличенный вес кошек
+        System.out.println("Вес кошки №1 " + cats[0].getWeight());
+        System.out.println("Вес кошки №7 " + cats[6].getWeight());
+        //перекормим кошку №2
+        while (!cats[1].getStatus().equals("Exploded")){
+            cats[1].feed(10.0);
+        }
+        //проверим вес перекормленной кошки, он должен быть больше 9000
+        System.out.println("Вес перекормленной кошки №2 " + cats[1].getWeight());
+        //замяукаем кошку №3 до смерти
+        while (!cats[2].getStatus().equals("Dead")){
+            cats[2].meow();
+        }
+        //проверим вес замяуканной кошки, он должен быть меньше 1000
+        System.out.println("Вес замяуканной кошки №3 " + cats[2].getWeight());
+
+        //УРОК №2
+        //создадим кошку и покормим ее кормом весом 150.00
+        Cat cat8 = new Cat();
+        cat8.feed(150.00);
+        //сходим в туалет 5 раз
+        int n=0;
+        while (n<5){
+            cat8.pee();
+            n++;
+        }
+        //распечатаем значение съеденного корма
+        System.out.println(cat8.getFoodEaten());
+
+        //УРОК №3
+
+        //количество кошек на момент запуска программы
+        System.out.println("Кошек всего было создано " + Cat.getCount());
+
+        /*проверим сколько кошек создано на данный момент
+                для этого применим метод aliveOrNot()
+                к каждой созданной кошке
+         */
+        for (int i=0;i<7;i++){
+            cats[i].aliveOrNot();
+        }
+        cat8.aliveOrNot();
+        //выведем на экран итоговое значение с помощью метода getCount()
+        System.out.println("Живых кошек на данный момент " + Cat.getCount());
+        //проверим, что нельзя кормить, поить и т.д. неживую кошку №3
+        cats[2].pee();
+        cats[2].feed(100.0);
+        cats[2].drink(100.0);
+        cats[2].meow();
+
+        //УРОК №4
+        //выведем на экран, созданные константы и ENUM
+        System.out.println(Cat.MAX_WEIGHT);
+        System.out.println(Cat.MIN_WEIGHT);
+        //Выведем ENUN с окрасами кошек
+        System.out.println(Arrays.toString(Cat.ColorOfTheCat.values()));
+
+        //УРОК №5
+        //создадим 9 кошку, используя новый конструктор
+        Cat cat9 = new Cat(3000.00);
+        System.out.println("Вес 9 кошки " + cat9.getWeight());
+        //используем метод getKitten() и выведем на экран вес котёнка
+        System.out.println("вес котёнка " + getKitten().getWeight());
+
+        //УРОК №6
+        //зададим и считаем окрас кошки
+        cat9.setColorOfTheCat(Cat.ColorOfTheCat.ChocolateTabby);
+        System.out.println("цвет кошки " + cat9.getColorOfTheCat());
+
+        //Урок №7
+        //проведем копирование кошки двумя способами
+        //конструктором
+        Cat cat10 = new Cat(cat9);
+        System.out.println("оригинал " + cat9.getWeight() + " " +
+                cat9.getColorOfTheCat());
+        System.out.println("копия " + cat10.getWeight() + " " +
+                cat10.getColorOfTheCat());
+        //методом
+        Cat cat11 = null;
+        try {
+            cat11 = (Cat)cat9.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("оригинал " + cat9.getWeight() + " " +
+                cat9.getColorOfTheCat());
+        System.out.println("копия " + cat11.getWeight() + " " +
+                cat11.getColorOfTheCat());
+        //изменим объект cat9 и проверим, что это не повлияло на
+        //cat10 и cat11
+        cat9.pee();
+        cat9.pee();
+        System.out.println("оригинал " + cat9.getWeight() + " " +
+                cat9.getColorOfTheCat());
+        System.out.println("копия cat10 " + cat10.getWeight() + " " +
+                cat10.getColorOfTheCat());
+        System.out.println("копия cat11 " + cat11.getWeight() + " " +
+                cat11.getColorOfTheCat());
+
+    }
+
+    //для 5 урока
+    private static Cat getKitten(){
+        return new Cat(1100.00);
+    }
+}
